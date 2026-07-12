0125c165e4e783f24e7f5b3b7fd8ee5a0da78c37
ishaikhamaan07 <ishaikhamaan07@gmail.com>
2026-07-12 14:18:09 +0530

    Added syntax highlighting for AI code responses

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,14 +1,23 @@
 import { assets } from "../assets/assets";
-
+import moment from 'moment'
+import { useEffect } from "react";
+import Markdown from 'react-markdown'
+import Prism from 'prismjs'
 
 const Message = ({message}) => {
+
+
+  useEffect(()=>{
+    Prism.highlightAll()
+  },[message.content])
+
   return (
     <div>
       {message.role === "user" ? (
         <div className="flex items-start justify-end my-4 gap-2">
             <div className="flex flex-col gap-2 p-2 px-4 bg-slate-50 dark:bg-[#57317C]/30 border border-[#80609F]/30 rounded-md max-w-2xl">
             <p className="text-sm dark:text-primary">{message.content}</p>
-            <span className="text-xs text-gray-400 dark:text-[#B1A6C0]">{message.timestamp}</span>
+            <span className="text-xs text-gray-400 dark:text-[#B1A6C0]">{moment(message.timestamp).fromNow()}</span>
             </div>
             <img src={assets.user_icon} alt="" className="w-8 rounded-full " />
         </div>
@@ -20,9 +29,9 @@ const Message = ({message}) => {
                 <img src={message.content} alt="" className="w-full max-w-md mt-2 rounded-md"/>
             ):
             (
-                <div className="text-sm dark:text-primary rest-tw">{message.content}</div>
+                <div className="text-sm dark:text-primary reset-tw"> <Markdown>{message.content}</Markdown> </div>
             )}
-            <span>{message.timestamp}</span>
+            <span className="text-xs text-gray-400 dark:text-[#B1A6C0]">{moment(message.timestamp).fromNow()}</span>
         </div>
       )
     }
